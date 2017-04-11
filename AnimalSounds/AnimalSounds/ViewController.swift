e14cb9ab75fb29d1a3e1fc4e3766b9c3432c0f31
Vu Trong Quang <vutrongquang@gmail.com>
2017-04-11 16:22:16 +0700

    Add project

---
+++ AnimalSounds/AnimalSounds/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  AnimalSounds
+//
+//  Created by Quang Vu on 4/8/17.
+//  Copyright © 2017 quangvt.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+    let mooSound = SimpleSound(named: "moo")
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
+    @IBAction func catButtonTapped(_ sender: Any) {
+        animalSoundLabel.text = "Meow!"
+        meowSound.play()
+    }
+    
+    @IBAction func dogButtonTapped(_ sender: Any) {
+        animalSoundLabel.text = "Woof!"
+        woofSound.play()
+    }
+    
+    @IBAction func cowButtonTapped(_ sender: Any) {
+        animalSoundLabel.text = "Moo!"
+        mooSound.play()
+    }
+    
+
+}
+
