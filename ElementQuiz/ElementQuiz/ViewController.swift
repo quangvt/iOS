6b8208832c9974d8293fb5068dfef19eb2a6b821
Vu Trong Quang <vutrongquang@gmail.com>
2017-04-11 16:23:05 +0700

    Add many projects

---
+++ ElementQuiz/ElementQuiz/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Quang Vu on 4/8/17.
+//  Copyright © 2017 quangvt.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateElement()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBAction func gotoNextElement(_ sender: Any) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
+    
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    func updateElement() {
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+    }
+    
+}
+
