597c5fae5f983b161ba33ef920d87a82dc3b9963
Quang Vu <vutrongquang@gmail.com>
2017-04-26 21:46:27 +0700

    TurnOnLed

---
+++ TurnOnLed/TurnOnLed/TurnOnLed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TurnOnLed
+//
+//  Created by Quang Vu on 4/26/17.
+//  Copyright © 2017 com.quangvt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var thanhvien = ["ong", "ba", "b", "me"]
+    var n = 5;
+    var _margin: CGFloat = 40
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        drawROwOfBall()
+        let timer =
+            NSTimer.scheduledTimerWithTimeInterval
+    }
+
+
+
+}
+
