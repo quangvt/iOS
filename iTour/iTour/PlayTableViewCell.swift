598ec641095181ead08058b2ce3338fe53f0e7e1
Quang Vu <vutrongquang@gmail.com>
2017-05-26 00:00:52 +0700

    update

---
+++ iTour/iTour/PlayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayTableViewCell.swift
+//  iTour
+//
+//  Created by Quang Vu on 5/23/17.
+//  Copyright © 2017 com.quangvt. All rights reserved.
+//
+
+import UIKit
+
+class PlayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
